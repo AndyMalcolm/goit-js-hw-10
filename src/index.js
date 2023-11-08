182f46f63291078084c64a6518781c1b68bdca64
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-11-08 13:38:17 +0200

    tak?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 //start here
-import axios from "axios";
+// import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU";
