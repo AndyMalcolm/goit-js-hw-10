796a3f3d02ae334104c5513762e8f5c5f6570d0a
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-11-17 01:05:14 +0200

    hm

---
+++ src/cat-api.js
@@ -0,0 +1,40 @@
+export function fetchBreeds(){
+    const BASE_URL = 'https://api.thecatapi.com/v1';
+    const END_POINT = '/breeds';
+    const PARAMS = `breeds`;
+    const url = BASE_URL + END_POINT + PARAMS;
+    const options = {
+      headers: {
+        'x-api-key':
+        'live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU'
+      },
+    };
+      return fetch(url, options).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
+  
+  export function fetchCatByBreed(breedId) {
+    const BASE_URL = 'https://api.thecatapi.com/v1';
+    const END_POINT = '/images/search';
+    const PARAMS = `?breed_ids=${breedId}`;
+    const url = BASE_URL + END_POINT + PARAMS;
+    const options = {
+      headers: {
+        'x-api-key':
+        'live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU'
+      },
+    };
+    return fetch(url, options).then(response => {
+        if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
+  
+  
+  
