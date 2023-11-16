c892947c30b4c601fa5881a6d6aa9841a0462365
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-11-17 01:07:00 +0200

    hm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 const theCatAPI = new TheCatAPI("live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU");
 axios.defaults.headers.common["x-api-key"] = "live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU";
-
+import Notiflix from 'notiflix';
