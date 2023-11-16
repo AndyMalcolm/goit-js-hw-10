44c7458b3872c55317d66ca006699cd6fc690b96
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-11-16 12:54:00 +0200

    hm

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-axios.defaults.headers.common["x-api-key"] = "live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU";
 import axios from "axios";
+const { TheCatAPI } = require("@thatapicompany/thecatapi");
+import { TheCatAPI } from "@thatapicompany/thecatapi";
+const theCatAPI = new TheCatAPI("live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU");
+axios.defaults.headers.common["x-api-key"] = "live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU";
+
