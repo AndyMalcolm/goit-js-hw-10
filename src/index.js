4c918c454709ba903732f1d935cc3be137abf435
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-11-17 01:08:40 +0200

    good?

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 const theCatAPI = new TheCatAPI("live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU");
 axios.defaults.headers.common["x-api-key"] = "live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU";
 import Notiflix from 'notiflix';
+//нотификс есть, код для загрузчика тоже
