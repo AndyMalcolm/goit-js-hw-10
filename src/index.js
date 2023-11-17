eda7535c4ef184adf5ea9c026e1e48903952e1de
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-11-18 00:18:19 +0200

    vrode da

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import axios from "axios";
-const theCatAPI = new TheCatAPI("live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU");
-axios.defaults.headers.common["x-api-key"] = "live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU";
+// axios.defaults.headers.common["x-api-key"] = "live_fJpEcmsalSnOTXMFW2gIUjht2meB9RjPKKjqwTJM4nwuPumBrJ4W2DyXwf7j0NpU";
 import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select'
+
 //нотификс есть, код для загрузчика тоже
